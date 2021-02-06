765044c01346e9562363e7487d2dd426e7c0ff67
Ridwan Ajibola <rajibola1997@gmail.com>
2021-02-06 15:01:28 +0100

    refactor: make all screen responsive

--- src/mock/cardData.js
+++ src/mock/cardData.js
@@ -9,16 +9,16 @@ export const Data = [
     ],
   },
   {
-    title: 'I have a registered business / charity with CAC',
-    head: 'As a registered business you’ll get:',
+    title: 'My business is not yet registered, I would like to register',
+    head: 'Everything you need to start your business',
     body: [
-      'Account in your business name',
-      'Send to and receive transfers from all Nigerian banks',
-      'Tools for business management',
+      'Registered business name with the CAC',
+      'Tax identification number',
+      'Your account will be automatically opened on completion',
     ],
   },
   {
-    title: 'I have a registered business / charity with CAC',
+    title: 'I’m a freelancer I do business in my personal name',
     head: 'As a registered business you’ll get:',
     body: [
       'Account in your business name',
